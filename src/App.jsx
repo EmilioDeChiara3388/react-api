1e1a6bfdf8718b23c1c4ac87c57bbf6985d72c08
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-20 16:22:20 +0100

    add: posts on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import AppHeader from './components/AppHeader'
 import './App.css'
 
 function App() {
 
+  const [postsData, setPostsData] = useState({})
+
+  function fetchData(url = "http://127.0.0.1:3000/") {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        setPostsData(data)
+      })
+  }
+
+  useEffect(fetchData, [])
 
   return (
     <>
       <AppHeader />
+      <section className='posts'>
+        <div className="container">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
+            {
+              postsData.data ?
+                postsData.data.map(post => (
+                  <div className="col" key={post.index}>
+                    <div className="card">
+                      <img src={'http://localhost:3000/imgs/posts/' + post.image} alt="" />
+                      <p>
+                        {post.title}
+                      </p>
+                    </div>
+                  </div>
+                )) :
+                <p>Nessun risultato</p>
+            }
+          </div>
+        </div>
+      </section>
     </>
   )
 }
